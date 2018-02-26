b04469a3919693b358446cd991276e85842e4692
ginnyqg <21312923+ginnyqg@users.noreply.github.com>
2018-02-25 23:30:16 -0500

    add main.html, main.pdf
    To compare change made for feature.R later

--- lib/feature.R
+++ lib/feature.R
@@ -7,30 +7,30 @@
 ### ADS Spring 2017
 
 feature <- function(img_dir, set_name, data_name="data", export=T){
-  
+
   ### Construct process features for training/testing images
   ### Sample simple feature: Extract row average raw pixel values as features
-  
+
   ### Input: a directory that contains images ready for processing
   ### Output: an .RData file contains processed features for the images
-  
+
   ### load libraries
   library("EBImage")
-  
+
   n_files <- length(list.files(img_dir))
-  
+
   ### determine img dimensions
   img0 <-  readImage(paste0(img_dir, data_name, 1, ".jpg"))
   mat1 <- as.matrix(img0)
   n_r  <- nrow(img0)
-  
+
   ### store vectorized pixel values of images
-  dat <- matrix(NA, n_files, n_r) 
+  dat <- matrix(NA, n_files, n_r)
   for(i in 1:n_files){
     img     <- readImage(paste0(img_dir, data_name, i, ".jpg"))
     dat[i,] <- rowMeans(img)
   }
-  
+
   ### output constructed features
   if(export){
     save(dat, file = paste0("../output/feature_", data_name, "_", set_name, ".RData"))
