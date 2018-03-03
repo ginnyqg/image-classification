ac086186cf4f4e5929f1ff2a576a4b693b2d5126
Ginny Gao <qinqingao1a@hotmail.com>
2018-03-03 17:14:48 -0500

    Add files via upload

---
+++ lib/feature.R
@@ -0,0 +1,59 @@
+#############################################################
+### Construct visual features for training/testing images ###
+#############################################################
+
+### Authors: Ginny Gao
+### Project 2
+### ADS Spring 2018
+
+#SIFT features with max, min sum per image, one row per image
+
+
+feature <- function(img_dir, set_name, data_name = "data", export = T){
+
+train_features_dir <- paste(experiment_dir, "train-features/", sep = "")
+
+n_files <- length(list.files(img_dir))
+
+load(paste(train_features_dir, 'pet', 1, '.jpg.sift.Rdata', sep = ''))
+
+set.seed(1234)
+bof <- kmeans(features, 10)
+
+dat <- cbind(t(bof$centers[1, ]), 
+             t(bof$centers[2, ]),
+             t(bof$centers[3, ]),
+             t(bof$centers[4, ]),
+             t(bof$centers[5, ]),
+             t(bof$centers[6, ]),
+             t(bof$centers[7, ]),
+             t(bof$centers[8, ]),
+             t(bof$centers[9, ]),
+             t(bof$centers[10, ])
+)
+
+for (i in 2 : n_files) {
+    	load(paste(train_features_dir, 'pet', i, '.jpg.sift.Rdata', sep = ''))
+        set.seed(1234)
+        bof <- kmeans(features, 10)
+    	dat <- rbind(dat, cbind(t(bof$centers[1, ]),
+                                t(bof$centers[2, ]),
+                                t(bof$centers[3, ]),
+                                t(bof$centers[4, ]),
+                                t(bof$centers[5, ]),
+                                t(bof$centers[6, ]),
+                                t(bof$centers[7, ]),
+                                t(bof$centers[8, ]),
+                                t(bof$centers[9, ]),
+                                t(bof$centers[10, ])
+    	                        ))
+	}
+
+
+### output constructed features
+if(export){
+    save(dat, file = paste0("../output/feature_", data_name, "_", set_name, ".RData"))
+    }
+return(dat)
+}
+
